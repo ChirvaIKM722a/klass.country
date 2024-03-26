1e70a142bea5cc11a617cc2b19208aac350e6d80
Daniil <chirvadana@gmail.com>
2024-03-27 00:34:00 +0300

    Добавьте файлы проекта.

---
+++ klass/Program.cs
@@ -0,0 +1,17 @@
+﻿using krainaukrauna1;
+
+krainaukrauna1.krainaukrauna1 country = new krainaukrauna1.krainaukrauna1("Бразилія", "федерація", 218000000, "бразильскій реал", "Бразиліа", "португальска" );
+
+country.DisplayInfo();
+
+country.Updatenasel(300000000);
+
+if (country.ProverkaNaselenia())
+{
+    Console.WriteLine("Населення стабільне");
+    
+}
+else
+{
+    Console.WriteLine("Бебі-бум в країні");
+}
---
+++ klass/krainaukrauna1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace krainaukrauna1
+{
+    public class krainaukrauna1
+    {
+        private string nazva; // названіє
+        private string GandY; // гос устройство
+        private long nasel; // насєленіє
+        private string val; // валюта
+        private string stol; // столиця
+        private string language; // язік
+
+        public krainaukrauna1(string nazva, string gandY, long nasel, string val, string stol, string language)
+        {
+            this.nazva = nazva;
+            GandY = gandY;
+            this.nasel = nasel;
+            this.val = val;
+            this.stol = stol;
+            this.language = language;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Nazva: {nazva}");
+            Console.WriteLine($"Gos Ustriq: {GandY}");
+            Console.WriteLine($"Naselennya: {nasel}");
+            Console.WriteLine($"Valuta: {val}");
+            Console.WriteLine($"Solucya: {stol}");
+            Console.WriteLine($"Mova: {language}");
+        }
+
+        public void Updatenasel(long nasel)
+        {
+            this.nasel = nasel;
+            Console.WriteLine("Naselenie izmenilos");
+        }
+
+        public bool ProverkaNaselenia()
+        {
+            return nasel < 290000000;
+        }
+
+    }
+}
